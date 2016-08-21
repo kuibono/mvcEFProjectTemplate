00a1d53bf7c09338a410301a7390250bff7349ad
cuibing <4264269>
2016-08-21 21:10:25 +0800

    authorize

---
+++ mvcEFProjectTemplate/1 Layers/1.4 Infrastructure/EF.Core/Domain/Authorize/MenuItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF.Core.Domain.Authorize
+{
+    public class MenuItem : IdentityEntity
+    {
+        [StringLength(255)]
+        public string Title { get; set; }
+
+        [StringLength(255)]
+        public string Target { get; set; }
+
+         [StringLength(2048)]
+        public string Url { get; set; }
+
+        [StringLength(255)]
+         public string Icon { get; set; }
+    }
+}
---
+++ mvcEFProjectTemplate/1 Layers/1.4 Infrastructure/EF.Core/Domain/Authorize/Role.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF.Core.Domain.Authorize
+{
+    public class Role : IdentityEntity
+    {
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [StringLength(2000)]
+        public string Description { get; set; }
+
+        public virtual ICollection<MenuItem> MenuItems { get; set; }
+    }
+}
